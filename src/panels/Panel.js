b26cfdf145d6ddba0c04e47ff8d2346bec27812e
DuarteMatos99 <duartematos1999@gmail.com>
2022-03-28 23:24:37 +0100

    new design panel

--- src/panels/Panel.js
+++ src/panels/Panel.js
@@ -4,7 +4,13 @@ import "../styles/panels.css";
 function Panel(props) {
     return (
         <div className="panel">
-            <h2>{props.title}</h2>
+            <div className="background-title">
+                {props.title.length > 17 ? (
+                    <h2>{`${props.title.slice(0, 17)}...`}</h2>
+                ) : (
+                    <h2>{props.title}</h2>
+                )}
+            </div>
             <p>date</p>
         </div>
     );
