26d80d733a99d5a793bdbfba44970395cc00d776
Duarte Matos <60755899+DuarteMatos99@users.noreply.github.com>
2022-03-31 00:16:41 +0100

    new scr

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -1,13 +1,11 @@
 import Navbar from "../navbar/Navbar";
 import RecentPanels from "../recent-panels/RecentPanels";
-import Separator from "../separator/Separator";
 
 function Dashboard() {
     return (
         <div>
             <Navbar />
             <RecentPanels />
-            <Separator />
         </div>
     );
 }
--- src/recent-panels/RecentPanels.js
+++ src/recent-panels/RecentPanels.js
@@ -1,6 +1,5 @@
 import { React, useState, useEffect } from "react";
 import Panel from "../panels/Panel";
-import Separator from "../separator/Separator"
 import "../styles/recentpanels.css";
 
 function RecentPanels() {
@@ -8,23 +7,29 @@ function RecentPanels() {
 
     useEffect(() => {
         // Update the document title using the browser API
-        fetch(`${process.env.REACT_APP_ENDPOINT}/note`)
+        fetch(`${process.env.REACT_APP_ENDPOINT}/note/getLatestFive`)
             .then((result) => result.json())
             .then((output) => {
                 setNotes(output);
+                console.log(output);
             })
             .catch((err) => console.error(err));
     }, []);
 
     return (
         <div className="recent-files">
-            <h4 className="title">Recent Files</h4>
+            <h3 className="title">Recent Files</h3>
             <div className="panels-area">
                 {notes.map((note) => {
-                    return <Panel title={note.title} key={note.id} />;
+                    return (
+                        <Panel
+                            title={note.title}
+                            key={note.id}
+                            creator={note.createdBy}
+                        />
+                    );
                 })}
             </div>
-            <br></br>
         </div>
     );
 }
